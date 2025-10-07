2e119aa5d09ddb6619a758b3d8532ef2f225a5aa
stevehatmakerjr <69125006+stevehatmakerjr@users.noreply.github.com>
2025-10-06 18:17:09 -0700

    Create Terms.jsx

---
+++ src/components/Terms.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+export default function Terms(){
+  return (
+    <div className="prose dark:prose-invert max-w-none">
+      <h1>Terms of Service</h1>
+      <p>Provided as‑is under the MIT License without warranty. You are responsible for how you use or modify this code. Avoid storing secrets in your browser.</p>
+    </div>
+  )
+}
